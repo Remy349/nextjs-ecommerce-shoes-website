1117eae6e208074890de4a6d241fe9449f9579d8
Remy349 <santoraga15@gmail.com>
2024-12-23 17:15:27 -0600

    remove: Remove files

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Button>Click me</Button>
-    </div>
-  );
-}
