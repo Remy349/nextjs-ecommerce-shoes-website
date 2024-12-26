e04b3d13e231d29027531bc477b26310e454e499
Remy349 <santoraga15@gmail.com>
2024-12-26 12:26:08 -0600

    feat: New di for user support

--- di/modules/user.module.ts
+++ di/modules/user.module.ts
@@ -3,6 +3,8 @@ import { DI_SYMBOLS } from "../types";
 import { UserRepositoryImpl } from "@/src/infrastructure/repositories/user.repository";
 import { isAdminCreatedUseCase } from "@/src/application/use-cases/user/is-admin-created.use-case";
 import { isAdminCreatedController } from "@/src/interface-adapters/controllers/user/is-admin-created.controller";
+import { createUserUseCase } from "@/src/application/use-cases/user/create-user.use-case";
+import { createUserController } from "@/src/interface-adapters/controllers/user/create-user.controller";
 
 export const createUserModule = () => {
   const userModule = createModule();
@@ -16,12 +18,22 @@ export const createUserModule = () => {
     .toHigherOrderFunction(isAdminCreatedUseCase, [
       DI_SYMBOLS.user.IUserRepository,
     ]);
+  userModule
+    .bind(DI_SYMBOLS.user.ICreateUserUseCase)
+    .toHigherOrderFunction(createUserUseCase, [
+      DI_SYMBOLS.user.IUserRepository,
+    ]);
 
   // CONTROLLERS
   userModule
     .bind(DI_SYMBOLS.user.IIsAdminCreatedController)
     .toHigherOrderFunction(isAdminCreatedController, [
       DI_SYMBOLS.user.IIsAdminCreatedUseCase,
+    ]);
+  userModule
+    .bind(DI_SYMBOLS.user.ICreateUserController)
+    .toHigherOrderFunction(createUserController, [
+      DI_SYMBOLS.user.ICreateUserUseCase,
     ]);
 
   return userModule;
--- di/types/user.type.ts
+++ di/types/user.type.ts
@@ -1,5 +1,7 @@
 import { IUserRepository } from "@/src/application/repositories/user.repository.interface";
+import { ICreateUserUseCase } from "@/src/application/use-cases/user/create-user.use-case";
 import { IIsAdminCreatedUseCase } from "@/src/application/use-cases/user/is-admin-created.use-case";
+import { ICreateUserController } from "@/src/interface-adapters/controllers/user/create-user.controller";
 import { IIsAdminCreatedController } from "@/src/interface-adapters/controllers/user/is-admin-created.controller";
 
 export const DI_USER_SYMBOLS = {
@@ -7,8 +9,10 @@ export const DI_USER_SYMBOLS = {
   IUserRepository: Symbol.for("IUserRepository"),
   // USE CASES
   IIsAdminCreatedUseCase: Symbol.for("IIsAdminCreatedUseCase"),
+  ICreateUserUseCase: Symbol.for("ICreateUserUseCase"),
   // CONTROLLERS
   IIsAdminCreatedController: Symbol.for("IIsAdminCreatedController"),
+  ICreateUserController: Symbol.for("ICreateUserController"),
 };
 
 export type DI_USER_RETURN_TYPES = {
@@ -16,6 +20,8 @@ export type DI_USER_RETURN_TYPES = {
   IUserRepository: IUserRepository;
   // USE CASES
   IIsAdminCreatedUseCase: IIsAdminCreatedUseCase;
+  ICreateUserUseCase: ICreateUserUseCase;
   // CONTROLLERS
   IIsAdminCreatedController: IIsAdminCreatedController;
+  ICreateUserController: ICreateUserController;
 };
