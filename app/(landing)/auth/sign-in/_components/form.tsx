6cca2e0f38d485da7068fac6576ebca0a867d66a
Remy349 <santoraga15@gmail.com>
2025-01-09 20:57:33 -0600

    feat: Add sign in page and form component

---
+++ app/(landing)/auth/sign-in/_components/form.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoaderCircle } from "lucide-react";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const FormSchema = z.object({
+  email: z.string().min(1, "Email is required").email("Not a valid email"),
+  password: z.string().min(1, "Password is required"),
+});
+
+type TFormSchema = z.infer<typeof FormSchema>;
+
+export const SignInForm = () => {
+  const form = useForm<TFormSchema>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: { email: "", password: "" },
+  });
+
+  const {
+    handleSubmit,
+    control,
+    formState: { isSubmitting },
+  } = form;
+
+  const onSubmit = async (formData: TFormSchema) => {};
+
+  return (
+    <Form {...form}>
+      <form onSubmit={handleSubmit(onSubmit)} className="grid gap-y-4">
+        <FormField
+          control={control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="text"
+                  autoComplete="off"
+                  disabled={isSubmitting}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <div className="flex items-center justify-between">
+                <FormLabel>Password</FormLabel>
+                <Button className="font-medium p-0" variant="link" asChild>
+                  <Link href="">Forgot your password?</Link>
+                </Button>
+              </div>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="password"
+                  autoComplete="off"
+                  disabled={isSubmitting}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="font-medium" disabled={isSubmitting}>
+          {isSubmitting ? (
+            <LoaderCircle className="size-5 animate-spin" />
+          ) : (
+            "Sign In"
+          )}
+        </Button>
+        <div className="text-center text-sm">
+          Do not have an account?{" "}
+          <Button className="font-medium p-0 underline" variant="link" asChild>
+            <Link href="">Create one</Link>
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
---
+++ app/(landing)/auth/sign-in/page.tsx
@@ -0,0 +1,50 @@
+import { Metadata } from "next";
+import { SignInForm } from "./_components/form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+};
+
+export default function Page() {
+  return (
+    <section className="pt-[3rem] pb-[2.5rem]">
+      <div className="px-6 mx-auto md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
+        <div className="lg:max-w-4xl lg:mx-auto">
+          <Card>
+            <CardContent className="p-0 grid md:grid-cols-2">
+              <div className="p-6">
+                <CardHeader className="p-0 pb-6">
+                  <CardTitle className="text-2xl">
+                    Welcome Back, Shopper!
+                  </CardTitle>
+                  <CardDescription>
+                    Sign in to access your orders, wishlist, and personalized
+                    shopping experience.
+                  </CardDescription>
+                </CardHeader>
+                <SignInForm />
+              </div>
+              <div className="hidden bg-muted md:block">
+                <picture>
+                  <img
+                    src="/bg-sign-in.jpg"
+                    alt="Background image"
+                    width={900}
+                    height={500}
+                  />
+                </picture>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </section>
+  );
+}
